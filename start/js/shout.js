622ee701c17a4c8604c711b82310cdd30c104509
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-10 08:52:50 -0500

    Creating the shout app

---
+++ start/js/shout.js
@@ -0,0 +1,7 @@
+const stringToShout = prompt('What do you want to shout?');
+
+const shout = stringToShout.toUpperCase();
+
+const shoutMessage = `<h2>The message I want to shout is ${shout}</h2>`;
+
+document.querySelector('main').innerHTML = shoutMessage;
--- start/js/template-literals.js
+++ start/js/template-literals.js
@@ -1,5 +1,5 @@
 const name = prompt('What is your name?');
 
-const message = "Hello" + name;
+const message = `Hello, ${name} It\'s ${2 * 3} o'clock`;
 
 console.log(message);
