3299409affaae4e3dec8f3c380da4064b9ea34de
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-10 10:24:30 -0500

    Finished booleans

--- js/booleans.js
+++ js/booleans.js
@@ -1,6 +1,13 @@
+let correctGuess = false;
+const number = 6;
+const guess = prompt('Guess a number between 1 and 10');
 
-if () {
-  console.log('The condition is true.');
+if (+guess === number) {
+  correctGuess = true;
+}
+
+if (correctGuess === true) {
+  console.log('you guessed the correct number');
 } else {
-  console.log('The condition is false.');
+  console.log('sorry try again');
 }
