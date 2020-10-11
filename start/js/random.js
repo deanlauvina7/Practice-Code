a5a9bf1e84b3b7596b6c9ad36f7077d7c0b2ee0f
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-11 08:34:50 -0500

    Practicing functions

--- start/js/random.js
+++ start/js/random.js
@@ -1,12 +1,2 @@
-// Collect input from a user
-let userInput = prompt('Please provide a number');
-// Convert the input to a number
-let userInputNumber = parseInt(userInput);
-// Use math.random() and the user's number to generate a random number
-let ranNum = Math.floor(Math.random() * userInputNumber) + 1;
-// Create a message displaying the random number
-document.querySelector('main').innerHTML = `<h2>${ranNum} is a number between 0 and ${userInputNumber}</h2>`;
+const randomNumber = Math.floor( Math.random() * 6 ) + 1;
 
-if (userInputNumber <= 0) {
-  alert('Sorry, please provide a number higher than 0');
-}
