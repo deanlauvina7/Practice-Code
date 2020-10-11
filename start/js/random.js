8de4d6a4b9f8439241547a9a95651b4d7af6766c
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-11 08:47:00 -0500

    Storing functions in a variable

--- start/js/random.js
+++ start/js/random.js
@@ -1,2 +1,7 @@
-const randomNumber = Math.floor( Math.random() * 6 ) + 1;
+function getRandomNumber(params) {
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
 
+  return randomNumber;
+};
+
+alert(getRandomNumber());
