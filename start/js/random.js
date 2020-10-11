2760db7ae9b5b45723a72fbeba6e6e291c435567
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-11 07:58:58 -0500

    Random number generator app

---
+++ start/js/random.js
@@ -0,0 +1,4 @@
+// Collect input from a user
+// Convert the input to a number
+// Use math.random() and the user's number to generate a random number
+// Create a message displaying the random number
