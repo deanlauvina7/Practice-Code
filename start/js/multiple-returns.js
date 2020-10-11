1099af7685d6a0a46f4ce96a9942525067b9deca
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-11 09:00:10 -0500

    Checking to see if a form field is empty

---
+++ start/js/multiple-returns.js
@@ -0,0 +1,15 @@
+function isFieldEmpty() {
+  const field = document.querySelector('#info');
+
+  if (field.value === '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const fieldTest = isFieldEmpty();
+
+if (fieldTest === true) {
+  alert('Please provide your information');
+} else {};
