0bd6c92f94d9e28292a873bf3009d426d7077077
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-14 08:31:36 -0500

    Finished html loops

--- start/js/html-loop.js
+++ start/js/html-loop.js
@@ -1,1 +1,13 @@
-const main = document.querySelector('main');
+const secretWord = 'tomato';
+let message = 'Access denied :(';
+
+for ( let i = 5; i >= 1; i-- ) {  
+  let guess = prompt(`Enter the secret word. You have ${i} tries.`);
+  if ( guess === secretWord ) {
+    message = 'Welcome to the secret loop world!';
+    break;
+  }
+}
+
+alert(message);
+main.innerHTML = html;
