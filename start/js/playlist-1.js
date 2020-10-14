f15cbd2cd6181a45300aa0690cac90ec7a84cd5b
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-14 09:32:38 -0500

    Created music playlist

--- start/js/playlist-1.js
+++ start/js/playlist-1.js
@@ -6,3 +6,14 @@ const playlist = [
   'Three Little Birds',
   'The Way You Look Tonight'
 ];
+
+function createListItems() { 
+  let items = '';
+  for (let i = 0; i < playlist.length; i++) {
+    items += `<li>${playlist[i]}</li>`;
+  }
+  return items;
+};
+
+document.querySelector('main').innerHTML = 
+`<ol>${createListItems(playlist)}</ol>`;
