b33a0e830680713eaef39a066b000498ce556f48
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-19 12:29:43 -0500

    Finish practice with manipulating the dom

--- vid-4-listening-for-events/start/app.js
+++ vid-4-listening-for-events/start/app.js
@@ -7,14 +7,23 @@ const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
+
+listDiv.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    let li = e.target.parentNode;
+    let ul = li.parentNode;
+    ul.removeChild(li);
+  }
+});
+
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -29,13 +38,13 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
+
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
 });
-  
-  
-  
-  
+
+if (condition) {
+
+}
