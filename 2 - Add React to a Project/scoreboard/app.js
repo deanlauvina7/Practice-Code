729ccd2d89a02bcd49e74abb87e9ba7a903bea90
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-28 21:49:18 -0500

    Practicing React

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,14 +1,46 @@
-const title = < h1 > My first React element! < /h1>
-const desc = < p > I just learned how to create a React element and render it to the DOM < /p>;
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  )
+};
 
-const header = React.createElement(
-  'header',
-  null,
-  title,
-  desc
-);
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Dean
+      </span>
+
+      <Counter />
+    </div>
+  );
+}
+
+const Counter = () => { 
+  return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+const App = () => { 
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/* Players list */ }
+      <Player />
+    </div>
+  );
+};
 
 ReactDOM.render(
-  header,
+  <App />,
   document.getElementById('root')
 );
