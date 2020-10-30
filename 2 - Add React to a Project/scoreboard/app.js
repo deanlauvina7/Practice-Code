76e06085a78f1528e173b56302d9da1802edf69b
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-30 09:16:32 -0500

    Learning about class

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -43,14 +43,16 @@ const Player = (props) => {
   );
 }
 
-const Counter = (props) => { 
-  return (
+class Counter extends React.Component { 
+  render() {
+    return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
+      <span className="counter-score">{this.props.score}</span>
         <button className="counter-action increment"> + </button>
     </div>
-  );
+    );
+  };
 }; 
 
 const App = (props) => { 
